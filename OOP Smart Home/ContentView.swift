94164c94ad62a711e041f8508456dbfd198a1429
Jack Ma <ma.j@etoncollege.org.uk>
2024-11-29 16:48:54 +0000

    Initial Commit

---
+++ OOP Smart Home/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OOP Smart Home
+//
+//  Created by Ma, Jack (IRG) on 29/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OOP Smart Home/OOP_Smart_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOP_Smart_HomeApp.swift
+//  OOP Smart Home
+//
+//  Created by Ma, Jack (IRG) on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct OOP_Smart_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
