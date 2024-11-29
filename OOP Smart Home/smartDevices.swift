1a2ef9ebf7114c7d13f03ef56da0f302e1736867
Jack Ma <ma.j@etoncollege.org.uk>
2024-11-29 17:11:05 +0000

    making progress

--- OOP Smart Home/smartDevices.swift
+++ OOP Smart Home/smartDevices.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+class smartDevices{
+    var name = String()
+    var connected = true
+    init(connected: Bool, name: String) {
+        self.connected = connected
+        self.name = name
+        
+    }
+}
+class speakers:smartDevices{
+    var volume = Int()
+    func changeVolume(change: Int){
+        volume = volume + change
+    }
+    func playSound() {
+
+    }
+
+    
+}
+class lights:smartDevices{
+    var brightness = Int()
+    var colour = String()
+    func changeBrightness(change: Int){
+        brightness = brightness + change
+    }
+    func changeColour(newcolour: String){
+        colour = newcolour
+    }
+    init(brightness: Int, colour: String) {
+        self.brightness = brightness
+        self.colour = colour
+    }
+    
+}
+class heating: smartDevices{
+    var temperature = Int()
+    func changeTemperature(change: Int){
+        temperature = temperature + change
+    }
+    init(temperature: Int) {
+        self.temperature = temperature
+        super.init(name:, connected:)
+    }
+}
